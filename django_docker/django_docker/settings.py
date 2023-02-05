84e4c7bc2b157367474239086272663a2479db89
skynet118 <nico_8830_3@hotmail.com>
2023-02-05 16:49:41 -0500

    new files in django docker api

--- django_docker/django_docker/settings.py
+++ django_docker/django_docker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sdev7+lngz$2+2^gwv0r__-3xiu&xk&fd2l*q+@ei7y^9yg4#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'api',
 ]
 
 MIDDLEWARE = [
